e74af06346d727a415cda17824126cb4865a070c
Md abdul Ripon <mdabdulripon@Mds-MacBook-Pro.lan>
2022-06-09 23:00:01 -0500

    create css stucture

--- apps/alligatorshop/src/app/app.component.ts
+++ apps/alligatorshop/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'alligatorshop-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'alligatorshop';
