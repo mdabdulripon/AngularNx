7a9aea4f894b6628978a67a101ee179bc19ac411
Md abdul Ripon <mdabdulripon@Mds-MacBook-Pro.lan>
2022-06-11 18:51:02 -0500

    create centralize enviorment file

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -10,8 +10,8 @@ import { AppComponent } from './app.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
-import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
+import { CategoriesListComponent } from './pages/categories/categories-list/categories-list.component';
+import { CategoriesFormComponent } from './pages/categories/categories-form/categories-form.component';
 import { CategoriesService } from '@alligatorspace/products';
 
 import { MatSliderModule } from '@angular/material/slider';
--- libs/products/src/lib/services/categories.service.ts
+++ libs/products/src/lib/services/categories.service.ts
@@ -2,32 +2,35 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { ICategory } from '../models/category';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
+  
+  private baseUrl = environment.baseUrl + 'categories';
 
   constructor(private http: HttpClient) { }
 
   getCategories() : Observable<ICategory[]> {
-    return this.http.get<ICategory[]>('http://localhost:3000/api/v1/categories/');
+    return this.http.get<ICategory[]>(this.baseUrl);
   }
 
   getCategory(categoryId: string) : Observable<ICategory>{
-    return this.http.get<ICategory>(`http://localhost:3000/api/v1/categories/${categoryId}`);
+    return this.http.get<ICategory>(`${this.baseUrl}/${categoryId}`);
   }
 
   createCategories(category: ICategory) : Observable<ICategory> {
-    return this.http.post<ICategory>('http://localhost:3000/api/v1/categories/', category);
+    return this.http.post<ICategory>(this.baseUrl, category);
   }
   
   updateCategories(category: ICategory) : Observable<ICategory> {
-    return this.http.put<ICategory>(`http://localhost:3000/api/v1/categories/${category.id}`, category);
+    return this.http.put<ICategory>(`${this.baseUrl}/${category.id}`, category);
   }
 
   deleteCategory(categoryId: string): Observable<any> {
-    return this.http.delete<any>(`http://localhost:3000/api/v1/categories/${categoryId}`);
+    return this.http.delete<any>(`${this.baseUrl}/${categoryId}`);
   }
 
 }
