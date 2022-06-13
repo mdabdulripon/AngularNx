7bd78237845809de670c44b70e527ff169ef4c4b
Md abdul Ripon <mdabdulripon@Mds-MacBook-Pro.lan>
2022-06-12 22:47:41 -0500

    add admin icon based on status

--- apps/admin/src/app/pages/users/users-list/users-list.component.ts
+++ apps/admin/src/app/pages/users/users-list/users-list.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class UsersListComponent implements OnInit {
 
-	displayedColumns: string[] = ['name', 'email', 'isAdmin', 'action'];
+	displayedColumns: string[] = ['name', 'email', 'isAdmin', 'country', 'action'];
 	dataSource: IUser[] = [];
 
 	constructor(
