63ac377d843b838178985da644e8b93b4a335000
Md abdul Ripon <mdabdulripon@Mds-MacBook-Pro.lan>
2022-06-17 00:13:38 -0500

    add seperate route for admin projects

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -18,7 +17,7 @@ import { ProductFormComponent } from './pages/products/product-form/product-form
 import { ProductListComponent } from './pages/products/product-list/product-list.component';
 import { UsersFormComponent } from './pages/users/users-form/users-form.component';
 import { UsersListComponent } from './pages/users/users-list/users-list.component';
-import { AuthGuard, JwtInterceptor, UsersModule, UsersService } from '@alligatorspace/users';
+import { JwtInterceptor, UsersModule, UsersService } from '@alligatorspace/users';
 import { OrdersListComponent } from './pages/orders/orders-list/orders-list.component';
 import { OrdersDetailComponent } from './pages/orders/orders-detail/orders-detail.component';
 
@@ -41,6 +40,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { AppRoutingModule } from './app-routing.module';
 
 
 
@@ -65,28 +65,6 @@ const MatModules = [
     MatAutocompleteModule
 ];
 
-const routes: Routes = [
-    {
-        path: '',
-        component: ShellComponent,
-        canActivate: [AuthGuard],
-        children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'categories', component: CategoriesListComponent },
-            { path: 'categories/form', component: CategoriesFormComponent },
-            { path: 'categories/form/:id', component: CategoriesFormComponent },
-            { path: 'products', component: ProductListComponent },
-            { path: 'products/form', component: ProductFormComponent },
-            { path: 'products/form/:id', component: ProductFormComponent },
-            { path: 'users', component: UsersListComponent },
-            { path: 'users/form', component: UsersFormComponent },
-            { path: 'users/form/:id', component: UsersFormComponent },
-            { path: 'orders', component: OrdersListComponent },
-            { path: 'orders/:id', component: OrdersDetailComponent },
-        ]
-    }
-];
-
 @NgModule({
     declarations: [
         AppComponent,
@@ -107,7 +85,7 @@ const routes: Routes = [
         BrowserAnimationsModule,
         HttpClientModule,
         ReactiveFormsModule,
-        RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
+        AppRoutingModule,
         StoreModule.forRoot({}, {}),
         UsersModule,
         ...MatModules
